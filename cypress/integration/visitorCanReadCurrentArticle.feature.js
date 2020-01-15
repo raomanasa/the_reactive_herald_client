74588208c1dbc0bcb2a3157883eb832ba03fc469
mantanquzzmo <quzzmo@hotmail.com>
2020-01-15 10:03:57 +0100

    Set up tests for DisplayMainArticle
    Co-authored-by: Manasa Rao <drmanasa18.rao@gmail.com>

---
+++ cypress/integration/visitorCanReadCurrentArticle.feature.js
@@ -0,0 +1,6 @@
+describe("User is shown current article"), () => {
+    it("shows the main article on page"), () => {
+        cy.visit("/");
+        cy.get("main-article-div").should("contain", "First article");
+      };
+  };
