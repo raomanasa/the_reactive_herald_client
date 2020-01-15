1dcc7069b86e0083727ae9c1442e1be7dc27d8a2
Manasa Rao <drmanasa18.rao@gmail.com>
2020-01-15 11:13:05 +0100

    adds fixatures and some more intergrations

--- cypress/integration/visitorCanReadCurrentArticle.feature.js
+++ cypress/integration/visitorCanReadCurrentArticle.feature.js
@@ -1,7 +1,15 @@
 describe("User is shown current article"), () => {
+  before(function() {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/api/v1/articles',
+      response: 'fixture:articles_show.json'
+    })
   it("shows the main article on page"), () => {
     cy.visit("/");
     cy.get("main-article-div").should("contain", "First article");
     };
-};
+  });
+}
 
