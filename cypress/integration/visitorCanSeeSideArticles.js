58eedc33d2d72373f69ce69df44f8434aab33017
mantanquzzmo <quzzmo@hotmail.com>
2020-01-16 16:55:25 +0100

    Adds feature test for side articles
    Co-authored-by: Johan Sundqvist <sundqvist.johan@outlook.com>

---
+++ cypress/integration/visitorCanSeeSideArticles.js
@@ -0,0 +1,16 @@
+describe("Visitor can see side articles", () => {
+  beforeEach(() => {
+    cy.server();
+  });
+
+  it("successfully", () => {
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/articles",
+      response: "fixture:side_articles_shown.json"
+    });
+    cy.visit("/");
+    cy.get("#main-article-div").should("contain", "Body 1");
+    cy.get("#side-articles-div").should("contain", "Body 2");
+  });
+});
