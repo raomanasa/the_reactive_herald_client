e1cfaa6633ace63cb798add154f2bf909aee3fbe
mantanquzzmo <quzzmo@hotmail.com>
2020-01-16 20:13:28 +0100

    Maps sideArticles in component and render variable
    Co-authored-by: Johan Sundqvist <sundqvist.johan@outlook.com>

--- src/components/DisplaySideArticles.jsx
+++ src/components/DisplaySideArticles.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const DisplaySideArticles = ({ sideArticles }) => {
-  return (
-    <div>
-      <div id="side-articles-div" key={sideArticles[0].id}>
-        <p id="article-title">{sideArticles[0].title}</p>
-        <p id="article-body">{sideArticles[0].body}</p>
-      </div>
-    </div>
-  )
-}
+  let articlesList = sideArticles.map(x => {
+    return (
+        <div id={`side-articles-div-${x.id}`} key={x.id}>
+          <p id="article-title">{x.title}</p>
+          <p id="article-body">{x.body}</p>
+        </div>
+    );
+  });
 
-export default DisplaySideArticles
+  return (
+    (articlesList)
+  );
+};
+
+export default DisplaySideArticles;
